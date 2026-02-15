1fe7befea30a87807654850dc747f0ebe2053b4b
OscarCabelloM <oscar.cabello@gmail.com>
2026-02-15 11:03:11 -0300

    Add files via upload

--- AppFinanzasFlet.py
+++ AppFinanzasFlet.py
@@ -1,7 +1,9 @@
+import time
 import flet as ft
 import sqlite3
 import re
 from datetime import datetime
+import asyncio
 
 DB_NAME = r"G:\Mi unidad\DBSqllite\DBAppFinanzasFlet.db"
 
@@ -70,8 +72,9 @@ def main(page: ft.Page):
 
     init_db()
 
-    page.title = "Control Finanzas"
-    page.scroll = ft.ScrollMode.AUTO
+    page.title = "CONTROL FINANZAS "
+    ###page.scroll = ft.ScrollMode.AUTO
+    page.window_maximized = True  # Maximiza la ventana
 
     editando_id = {"id": None}
     borrar_id = {"id": None}
@@ -79,9 +82,9 @@ def main(page: ft.Page):
     # ---------- FUNCIONES ----------
 
     def mostrar_mensaje(msg):
-        page.snack_bar = ft.SnackBar(ft.Text(msg))
-        page.snack_bar.open = True
+        texto_mensaje.value=msg
         page.update()
+
 
     def formato_miles(valor):
         return f"{int(valor):,}".replace(",", ".")
@@ -89,40 +92,66 @@ def main(page: ft.Page):
     def formato_fecha(valor):
         return re.sub(r"(\d{2})(\d{2})(\d{4})", r"\1/\2/\3", valor)
 
-    def confirmar_borrar():
-        if boton_confirmar.on_click:
-            eliminar_registro(borrar_id["id"])
-            cargar_tabla()
-            mostrar_mensaje("Registro eliminado")
 
-    # ---------- VALIDACIONES ----------
     def validar_descripcion(txt):
         return bool(re.fullmatch(r"[A-Z0-9.,+\- ]+", txt))
+    
+    def validar_fecha(fecha_str):
+        # 1️⃣ Validar formato exacto dd-mm-yyyy
+        patron = r"^\d{2}-\d{2}-\d{4}$"
+        
+        if not re.match(patron, fecha_str):
+            return False
+        
+        # 2️⃣ Validar que la fecha exista realmente
+        try:
+            datetime.strptime(fecha_str, "%d-%m-%Y")
+            return True
+        except ValueError:
+            return False
+            
+    def validar_monto(monto):
+        try:
+            valor = float(monto)
+            return valor > 0
+        except ValueError:
+            return False
+    # ---------- VALIDACIONES ----------
+
 
     # ---------- GUARDAR ----------
+    def Borrar_registro(e):
+        if not texto_id.value:
+            mostrar_mensaje(">> ERROR: Seleccionar Registro")
+            return
+        id=texto_id.value
+        eliminar_registro(id)
+        mostrar_mensaje(">> OK: Registro Eliminado")
+        limpiar_campos()
+        cargar_tabla()
+
+
     def guardar_registro(e):
-
-        monto = re.sub(r"\D", "", input_monto.value)
-
-        if not monto:
-            mostrar_mensaje("Monto inválido")
+        monto = int(input_monto.value.replace(".", ""))
+        if not validar_monto(monto):
+            mostrar_mensaje(">> ERROR: Monto debe ser numerico y Mayor a 0")
             return
 
         if not input_descripcion.value.strip():
-            mostrar_mensaje("Descripción obligatoria")
+            mostrar_mensaje(">> ERROR: Descripción Obligatoria")
             return
 
         if not validar_descripcion(input_descripcion.value):
-            mostrar_mensaje("Descripción inválida")
+            mostrar_mensaje(">> ERROR: Descripción Inválida")
             return
 
-        if not input_fecha.value:
-            mostrar_mensaje("Seleccione fecha")
+        if not validar_fecha(input_fecha.value):
+            mostrar_mensaje(">> ERROR: Fecha Inválida dd-mm-yyyy")
             return
 
         data = (
             input_tipo.value,
-            int(monto),
+            monto,
             input_cuenta.value,
             input_categoria.value,
             input_descripcion.value,
@@ -131,23 +160,30 @@ def main(page: ft.Page):
 
         if editando_id["id"] is None:
             insertar_registro(data)
-            mostrar_mensaje("Registro guardado")
+            mostrar_mensaje(">> OK: Registro Guardado")
         else:
             actualizar_registro(data + (editando_id["id"],))
             editando_id["id"] = None
-            mostrar_mensaje("Registro actualizado")
+            mostrar_mensaje(">> OK: Registro Actualizado")
 
-        limpiar()
+        limpiar_campos()
         cargar_tabla()
 
+
     # ---------- LIMPIAR ----------
-    def limpiar():
+    def limpiar_campos():
+        texto_id.value=""
         input_tipo.value = "GASTO"
-        input_monto.value = ""
+        input_monto.value = "0"
         input_cuenta.value = "EFECTIVO"
         input_categoria.value = "SIN CATEGORIA"
         input_descripcion.value = ""
-        input_fecha.value = datetime.now().strftime("%Y-%m-%d")
+        input_fecha.value = datetime.now().strftime("%d-%m-%Y")
+        page.update()
+
+    def limpiar_campos2():
+        limpiar_campos()
+        texto_mensaje.value=""
         page.update()
 
     # ---------- TABLA ----------
@@ -160,6 +196,7 @@ def main(page: ft.Page):
             rid = row[0]
 
             def editar_click(e, r=row):
+                texto_id.value=r[0]               
                 editando_id["id"] = r[0]
                 input_tipo.value = r[1]
                 input_monto.value = formato_miles(r[2])
@@ -168,10 +205,6 @@ def main(page: ft.Page):
                 input_descripcion.value = r[5]
                 input_fecha.value = r[6]
                 page.update()
-
-            def borrar_click(e, rid=rid):
-                eliminar_registro(rid)
-                cargar_tabla()
 
             tabla.controls.append(
                 ft.Container(
@@ -187,7 +220,6 @@ def main(page: ft.Page):
                         ft.Text(row[5], width=260),
                         ft.Text(row[6], width=100),
                         ft.IconButton(ft.Icons.EDIT, on_click=editar_click),
-                        ft.IconButton(ft.Icons.DELETE, on_click=borrar_click),
                     ])
                 )
             )
@@ -196,10 +228,31 @@ def main(page: ft.Page):
 
 
     # ---------- CONTROLES ----------
+    # ID
+
+    texto_id = ft.Text("", size=24, weight="bold")
+
+    # FECHA
+    input_fecha = ft.TextField(
+        width=140,
+        value=datetime.now().strftime("%Y-%m-%d"),
+        label="Fecha"
+    )
+    
+    def fecha_change(e):
+        valor = re.sub(r"\D", "", e.control.value)
+        if valor:
+            e.control.value = formato_fecha(valor)
+        else:
+            e.control.value = ""
+        page.update()
+
+        input_fecha.on_change = fecha_change
+
 
     input_tipo = ft.Dropdown(
         label="Tipo",
-        width=200,
+        width=140,
         value="GASTO",
         options=[
             ft.dropdown.Option("GASTO"),
@@ -223,7 +276,7 @@ def main(page: ft.Page):
 
     input_cuenta = ft.Dropdown(
         label="Cuenta",
-        width=200,
+        width=140,
         value="EFECTIVO",
         options=[
             ft.dropdown.Option("EFECTIVO"),
@@ -234,17 +287,20 @@ def main(page: ft.Page):
 
     input_categoria = ft.Dropdown(
         label="Categoría",
-        width=200,
+        width=350,
         value="SIN CATEGORIA",
         options=[
             ft.dropdown.Option("SIN CATEGORIA"),
-            ft.dropdown.Option("A"),
-            ft.dropdown.Option("B"),
-            ft.dropdown.Option("C"),
+            ft.dropdown.Option("CATEGORIA-A"),
+            ft.dropdown.Option("CATEGORIA-B"),
+            ft.dropdown.Option("CATEGORIA-C"),
         ]
     )
 
-    input_descripcion = ft.TextField(label="Descripción", width=410, max_length=40)
+    input_descripcion = ft.TextField(
+        label="Descripción", 
+        width=500, 
+        max_length=40)
 
     def descripcion_change(e):
         e.control.value = e.control.value.upper()
@@ -252,40 +308,30 @@ def main(page: ft.Page):
 
     input_descripcion.on_change = descripcion_change
 
-    # FECHA
-    input_fecha = ft.TextField(
-        width=200,
-        value=datetime.now().strftime("%Y-%m-%d"),
-        label="Fecha"
-    )
-    
-    def fecha_change(e):
-        valor = re.sub(r"\D", "", e.control.value)
-        if valor:
-            e.control.value = formato_fecha(valor)
-        else:
-            e.control.value = ""
-        page.update()
 
-        input_fecha.on_change = fecha_change
-
-
+    boton_limpiar = ft.FilledButton("Limpiar", on_click=limpiar_campos2)
     boton_guardar = ft.FilledButton("Guardar", on_click=guardar_registro)
-    boton_confirmar = ft.FilledButton("Confirmar")
+    boton_eliminar = ft.FilledButton("Eliminar", on_click=Borrar_registro)
+
+    # MENSAJE
+
+    texto_mensaje = ft.Text("",size=20)
+
 
     # ---------- LAYOUT ----------
     page.add(
-        ft.Text("Registro Financiero", size=24, weight="bold"),
-        ft.Row([input_tipo, input_monto]),
+
+        ft.Row([ft.Text("Registro Financiero ", size=24, weight="bold"),texto_id]),
+        ft.Row([input_fecha, input_tipo, input_monto]),
         ft.Row([input_cuenta, input_categoria]),
-        input_descripcion,
-        ft.Row([input_fecha, boton_guardar, boton_confirmar]),
+        ft.Row([input_descripcion]),
         ft.Divider(),
-        ft.Text("Últimos 5 registros", size=20),
+        ft.Row([boton_limpiar, boton_guardar, boton_eliminar,texto_mensaje]),
+        ft.Divider(),
         tabla
     )
 
-    limpiar()
+    limpiar_campos()
     cargar_tabla()
 
 
